ea2006c74435cf6764040bb2583ae13bcad80b3d
AndreevDan93 <andreevdan93@gmail.com>
2022-06-20 12:28:51 +0500

    nextLevel

--- src/TrollFace.java
+++ src/TrollFace.java
@@ -9,34 +9,66 @@ public class TrollFace {
     static Image t4;
     static Image t5;
     static Image t6;
+    static Image t7;
+    static Image t8;
+    static Image t9;
+    static Image t10;
+    static Image t11;
+    static Image t12;
     static Image center;
     static float trollFaceX1 = 0;
-    static float trollFaceY1 = 0;
     static float trollFaceX2 = 0;
-    static float trollFaceY2 = 0;
     static float trollFaceX3 = 0;
-    static float trollFaceY3 = 0;
     static float trollFaceX4 = 0;
-    static float trollFaceY4 = 0;
     static float trollFaceX5 = 0;
-    static float trollFaceY5 = 0;
     static float trollFaceX6 = 0;
-    static float trollFaceY6 = 0;
+    static float trollFaceX7 = 0;
+    static float trollFaceX8 = 0;
+    static float trollFaceX9 = 0;
+    static float trollFaceX10 = 0;
+    static float trollFaceX11 = 0;
+    static float trollFaceX12 = 0;
 
-    static float trollFaceV1 = 4;
-    static float trollFaceV2 = 3;
-    static float trollFaceV3 = 2;
-    static float trollFaceV4 = 4;
-    static float trollFaceV5 = 3;
-    static float trollFaceV6 = 2;
+
+    static float trollFaceY1 = 0;
+    static float trollFaceY2 = 0;
+    static float trollFaceY3 = 0;
+    static float trollFaceY4 = 0;
+    static float trollFaceY5 = 0;
+    static float trollFaceY6 = 0;
+    static float trollFaceY7 = 0;
+    static float trollFaceY8 = 0;
+    static float trollFaceY9 = 0;
+    static float trollFaceY10 = 0;
+    static float trollFaceY11 = 0;
+    static float trollFaceY12 = 0;
+
+    static float trollFaceV1 = 3;
+    static float trollFaceV2 = 2;
+    static float trollFaceV3 = 1;
+    static float trollFaceV4 = 3;
+    static float trollFaceV5 = 2;
+    static float trollFaceV6 = 1;
+    static float trollFaceV7 = 3;
+    static float trollFaceV8 = 2;
+    static float trollFaceV9 = 1;
+    static float trollFaceV10 = 3;
+    static float trollFaceV11 = 2;
+    static float trollFaceV12 = 1;
 
 
     static float trollFaceT1 = 0;
-    static float trollFaceT2 = 60;
-    static float trollFaceT3 = 120;
-    static float trollFaceT4 = 180;
-    static float trollFaceT5 = 240;
-    static float trollFaceT6 = 300;
+    static float trollFaceT2 = 30;
+    static float trollFaceT3 = 60;
+    static float trollFaceT4 = 90;
+    static float trollFaceT5 = 120;
+    static float trollFaceT6 = 150;
+    static float trollFaceT7 = 180;
+    static float trollFaceT8 = 210;
+    static float trollFaceT9 = 240;
+    static float trollFaceT10 = 270;
+    static float trollFaceT11 = 300;
+    static float trollFaceT12 = 330;
 
 
     static float trollFaceR1 = 150;
@@ -45,53 +77,29 @@ public class TrollFace {
     static float trollFaceR4 = 150;
     static float trollFaceR5 = 300;
     static float trollFaceR6 = 450;
+    static float trollFaceR7 = 150;
+    static float trollFaceR8 = 300;
+    static float trollFaceR9 = 450;
+    static float trollFaceR10 = 150;
+    static float trollFaceR11 = 300;
+    static float trollFaceR12 = 450;
 
 
-    static {
-        try {
-            t1 = ImageIO.read(Window.class.getResourceAsStream("1.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    static {
-        try {
-            t2 = ImageIO.read(Window.class.getResourceAsStream("2.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    static {
-        try {
-            t3 = ImageIO.read(Window.class.getResourceAsStream("3.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    static {
-        try {
-            t4 = ImageIO.read(Window.class.getResourceAsStream("4.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    static {
-        try {
-            t5 = ImageIO.read(Window.class.getResourceAsStream("5.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    static {
-        try {
-            t6 = ImageIO.read(Window.class.getResourceAsStream("6.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
     static {
         try {
             center = ImageIO.read(Window.class.getResourceAsStream("centr.png"));
+            t1 = ImageIO.read(Window.class.getResourceAsStream("1.png"));
+            t2 = ImageIO.read(Window.class.getResourceAsStream("2.png"));
+            t3 = ImageIO.read(Window.class.getResourceAsStream("3.png"));
+            t4 = ImageIO.read(Window.class.getResourceAsStream("4.png"));
+            t5 = ImageIO.read(Window.class.getResourceAsStream("5.png"));
+            t6 = ImageIO.read(Window.class.getResourceAsStream("6.png"));
+            t7 = ImageIO.read(Window.class.getResourceAsStream("1.png"));
+            t8 = ImageIO.read(Window.class.getResourceAsStream("2.png"));
+            t9 = ImageIO.read(Window.class.getResourceAsStream("3.png"));
+            t10 = ImageIO.read(Window.class.getResourceAsStream("4.png"));
+            t11 = ImageIO.read(Window.class.getResourceAsStream("5.png"));
+            t12 = ImageIO.read(Window.class.getResourceAsStream("6.png"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/Window.java
+++ src/Window.java
@@ -1,3 +1,5 @@
+
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -33,6 +35,7 @@ public class Window extends JFrame {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
 
+
             Window.move(g);
             repaint();
 
@@ -52,7 +55,7 @@ public class Window extends JFrame {
         TrollFace.trollFaceX1 = (float) (TrollFace.trollFaceR1 * Math.cos(Math.toRadians(TrollFace.trollFaceT1)) + 435);
         TrollFace.trollFaceY1 = (float) (TrollFace.trollFaceR1 * Math.sin(Math.toRadians(TrollFace.trollFaceT1)) + 435);
 
-        TrollFace.trollFaceT2 = TrollFace.trollFaceT2 + deltaTime * TrollFace.trollFaceV2;
+        TrollFace.trollFaceT2 = TrollFace.trollFaceT2 - deltaTime * TrollFace.trollFaceV2;
         TrollFace.trollFaceX2 = (float) (TrollFace.trollFaceR2 * Math.cos(Math.toRadians(TrollFace.trollFaceT2)) + 435);
         TrollFace.trollFaceY2 = (float) (TrollFace.trollFaceR2 * Math.sin(Math.toRadians(TrollFace.trollFaceT2)) + 435);
 
@@ -60,7 +63,7 @@ public class Window extends JFrame {
         TrollFace.trollFaceX3 = (float) (TrollFace.trollFaceR3 * Math.cos(Math.toRadians(TrollFace.trollFaceT3)) + 435);
         TrollFace.trollFaceY3 = (float) (TrollFace.trollFaceR3 * Math.sin(Math.toRadians(TrollFace.trollFaceT3)) + 435);
 
-        TrollFace.trollFaceT4 = TrollFace.trollFaceT4 + deltaTime * TrollFace.trollFaceV4;
+        TrollFace.trollFaceT4 = TrollFace.trollFaceT4 - deltaTime * TrollFace.trollFaceV4;
         TrollFace.trollFaceX4 = (float) (TrollFace.trollFaceR4 * Math.cos(Math.toRadians(TrollFace.trollFaceT4)) + 435);
         TrollFace.trollFaceY4 = (float) (TrollFace.trollFaceR4 * Math.sin(Math.toRadians(TrollFace.trollFaceT4)) + 435);
 
@@ -68,11 +71,33 @@ public class Window extends JFrame {
         TrollFace.trollFaceX5 = (float) (TrollFace.trollFaceR5 * Math.cos(Math.toRadians(TrollFace.trollFaceT5)) + 435);
         TrollFace.trollFaceY5 = (float) (TrollFace.trollFaceR5 * Math.sin(Math.toRadians(TrollFace.trollFaceT5)) + 435);
 
-        TrollFace.trollFaceT6 = TrollFace.trollFaceT6 + deltaTime * TrollFace.trollFaceV6;
+        TrollFace.trollFaceT6 = TrollFace.trollFaceT6 - deltaTime * TrollFace.trollFaceV6;
         TrollFace.trollFaceX6 = (float) (TrollFace.trollFaceR6 * Math.cos(Math.toRadians(TrollFace.trollFaceT6)) + 435);
         TrollFace.trollFaceY6 = (float) (TrollFace.trollFaceR6 * Math.sin(Math.toRadians(TrollFace.trollFaceT6)) + 435);
 
+        TrollFace.trollFaceT7 = TrollFace.trollFaceT7 + deltaTime * TrollFace.trollFaceV7;
+        TrollFace.trollFaceX7 = (float) (TrollFace.trollFaceR7 * Math.cos(Math.toRadians(TrollFace.trollFaceT7)) + 435);
+        TrollFace.trollFaceY7 = (float) (TrollFace.trollFaceR7 * Math.sin(Math.toRadians(TrollFace.trollFaceT7)) + 435);
 
+        TrollFace.trollFaceT8 = TrollFace.trollFaceT8 - deltaTime * TrollFace.trollFaceV8;
+        TrollFace.trollFaceX8 = (float) (TrollFace.trollFaceR8 * Math.cos(Math.toRadians(TrollFace.trollFaceT8)) + 435);
+        TrollFace.trollFaceY8 = (float) (TrollFace.trollFaceR8 * Math.sin(Math.toRadians(TrollFace.trollFaceT8)) + 435);
+
+        TrollFace.trollFaceT9 = TrollFace.trollFaceT9 + deltaTime * TrollFace.trollFaceV9;
+        TrollFace.trollFaceX9 = (float) (TrollFace.trollFaceR9 * Math.cos(Math.toRadians(TrollFace.trollFaceT9)) + 435);
+        TrollFace.trollFaceY9 = (float) (TrollFace.trollFaceR9 * Math.sin(Math.toRadians(TrollFace.trollFaceT9)) + 435);
+
+        TrollFace.trollFaceT10 = TrollFace.trollFaceT10 - deltaTime * TrollFace.trollFaceV10;
+        TrollFace.trollFaceX10 = (float) (TrollFace.trollFaceR10 * Math.cos(Math.toRadians(TrollFace.trollFaceT10)) + 435);
+        TrollFace.trollFaceY10 = (float) (TrollFace.trollFaceR10 * Math.sin(Math.toRadians(TrollFace.trollFaceT10)) + 435);
+
+        TrollFace.trollFaceT11 = TrollFace.trollFaceT11 + deltaTime * TrollFace.trollFaceV11;
+        TrollFace.trollFaceX11 = (float) (TrollFace.trollFaceR11 * Math.cos(Math.toRadians(TrollFace.trollFaceT11)) + 435);
+        TrollFace.trollFaceY11 = (float) (TrollFace.trollFaceR11 * Math.sin(Math.toRadians(TrollFace.trollFaceT11)) + 435);
+
+        TrollFace.trollFaceT12 = TrollFace.trollFaceT12 - deltaTime * TrollFace.trollFaceV12;
+        TrollFace.trollFaceX12 = (float) (TrollFace.trollFaceR12 * Math.cos(Math.toRadians(TrollFace.trollFaceT12)) + 435);
+        TrollFace.trollFaceY12 = (float) (TrollFace.trollFaceR12 * Math.sin(Math.toRadians(TrollFace.trollFaceT12)) + 435);
 
 
         g.drawImage(TrollFace.t1, (int) TrollFace.trollFaceX1, (int) TrollFace.trollFaceY1, null);
@@ -81,33 +106,44 @@ public class Window extends JFrame {
         g.drawImage(TrollFace.t4, (int) TrollFace.trollFaceX4, (int) TrollFace.trollFaceY4, null);
         g.drawImage(TrollFace.t5, (int) TrollFace.trollFaceX5, (int) TrollFace.trollFaceY5, null);
         g.drawImage(TrollFace.t6, (int) TrollFace.trollFaceX6, (int) TrollFace.trollFaceY6, null);
+        g.drawImage(TrollFace.t7, (int) TrollFace.trollFaceX7, (int) TrollFace.trollFaceY7, null);
+        g.drawImage(TrollFace.t8, (int) TrollFace.trollFaceX8, (int) TrollFace.trollFaceY8, null);
+        g.drawImage(TrollFace.t9, (int) TrollFace.trollFaceX9, (int) TrollFace.trollFaceY9, null);
+        g.drawImage(TrollFace.t10, (int) TrollFace.trollFaceX10, (int) TrollFace.trollFaceY10, null);
+        g.drawImage(TrollFace.t11, (int) TrollFace.trollFaceX11, (int) TrollFace.trollFaceY11, null);
+        g.drawImage(TrollFace.t12, (int) TrollFace.trollFaceX12, (int) TrollFace.trollFaceY12, null);
 
-        if (TrollFace.trollFaceX1 < 1000) {
-            TrollFace.trollFaceX1 = TrollFace.trollFaceX1 + TrollFace.trollFaceV1 * deltaTime;
-            TrollFace.trollFaceY1 = 0;
-        } else TrollFace.trollFaceX1 = 0;
 
-        if (TrollFace.trollFaceX2 < 1000) {
-            TrollFace.trollFaceX2 = TrollFace.trollFaceX2 + TrollFace.trollFaceV2 * deltaTime;
-            TrollFace.trollFaceY2 = 150;
-        } else TrollFace.trollFaceX2 = 0;
+        TrollFace.trollFaceX1 = TrollFace.trollFaceX1 + TrollFace.trollFaceV1 * deltaTime;
+        TrollFace.trollFaceY1 = 0;
 
-        if (TrollFace.trollFaceX3 < 1000) {
-            TrollFace.trollFaceX3 = TrollFace.trollFaceX3 + TrollFace.trollFaceV3 * deltaTime;
-            TrollFace.trollFaceY3 = 300;
-        } else TrollFace.trollFaceX3 = 0;
+        TrollFace.trollFaceX2 = TrollFace.trollFaceX2 + TrollFace.trollFaceV2 * deltaTime;
+        TrollFace.trollFaceY2 = 150;
 
-       
+        TrollFace.trollFaceX3 = TrollFace.trollFaceX3 + TrollFace.trollFaceV3 * deltaTime;
+        TrollFace.trollFaceY3 = 250;
 
-        if (TrollFace.trollFaceX5 < 1000) {
-            TrollFace.trollFaceX5 = TrollFace.trollFaceX5 + TrollFace.trollFaceV5 * deltaTime;
-            TrollFace.trollFaceY5 = 600;
-        } else TrollFace.trollFaceX5 = 0;
+        TrollFace.trollFaceX5 = TrollFace.trollFaceX5 + TrollFace.trollFaceV5 * deltaTime;
+        TrollFace.trollFaceY5 = 600;
 
-        if (TrollFace.trollFaceX6 < 1000) {
-            TrollFace.trollFaceX6 = TrollFace.trollFaceX6 + TrollFace.trollFaceV6 * deltaTime;
-            TrollFace.trollFaceY6 = 750;
-        } else TrollFace.trollFaceX6 = 0;
+        TrollFace.trollFaceX6 = TrollFace.trollFaceX6 + TrollFace.trollFaceV6 * deltaTime;
+        TrollFace.trollFaceY6 = 750;
+
+        TrollFace.trollFaceX7 = TrollFace.trollFaceX7 - TrollFace.trollFaceV7 * deltaTime;
+        TrollFace.trollFaceY7 = 0;
+
+        TrollFace.trollFaceX8 = TrollFace.trollFaceX8 - TrollFace.trollFaceV8 * deltaTime;
+        TrollFace.trollFaceY8 = 150;
+
+        TrollFace.trollFaceX9 = TrollFace.trollFaceX9 - TrollFace.trollFaceV9 * deltaTime;
+        TrollFace.trollFaceY9 = 250;
+
+        TrollFace.trollFaceX11 = TrollFace.trollFaceX11 - TrollFace.trollFaceV11 * deltaTime;
+        TrollFace.trollFaceY11 = 600;
+
+        TrollFace.trollFaceX12 = TrollFace.trollFaceX12 - TrollFace.trollFaceV12 * deltaTime;
+        TrollFace.trollFaceY12 = 750;
+
 
         g.drawImage(TrollFace.t1, (int) TrollFace.trollFaceX1, (int) TrollFace.trollFaceY1, null);
         g.drawImage(TrollFace.t2, (int) TrollFace.trollFaceX2, (int) TrollFace.trollFaceY2, null);
@@ -115,6 +151,12 @@ public class Window extends JFrame {
         g.drawImage(TrollFace.t4, (int) TrollFace.trollFaceX4, (int) TrollFace.trollFaceY4, null);
         g.drawImage(TrollFace.t5, (int) TrollFace.trollFaceX5, (int) TrollFace.trollFaceY5, null);
         g.drawImage(TrollFace.t6, (int) TrollFace.trollFaceX6, (int) TrollFace.trollFaceY6, null);
+        g.drawImage(TrollFace.t7, (int) TrollFace.trollFaceX7, (int) TrollFace.trollFaceY7, null);
+        g.drawImage(TrollFace.t8, (int) TrollFace.trollFaceX8, (int) TrollFace.trollFaceY8, null);
+        g.drawImage(TrollFace.t9, (int) TrollFace.trollFaceX9, (int) TrollFace.trollFaceY9, null);
+        g.drawImage(TrollFace.t10, (int) TrollFace.trollFaceX10, (int) TrollFace.trollFaceY10, null);
+        g.drawImage(TrollFace.t11, (int) TrollFace.trollFaceX11, (int) TrollFace.trollFaceY11, null);
+        g.drawImage(TrollFace.t12, (int) TrollFace.trollFaceX12, (int) TrollFace.trollFaceY12, null);
 
 
     }
